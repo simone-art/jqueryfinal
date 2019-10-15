1799bbeec8363cc538d0029a06a626c2aa1c69c3
simone-art <simoneperiodista3@gmail.com>
2019-10-15 14:05:39 -0300

    Fiz o exercicio 4 com o efeito slide, mais ainda faltou colocar um efeito slide diferente em cada card

---
+++ exercicios/js/page4.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $(".btn-primary").click(function(){
+        $(".Tenya").slideDown(1000);   
+    })
+
+    $(".btn-primary").click(function(){
+        $(".Momo").slideDown(5000);   
+    })
+
+    $(".btn-primary").click(function(){
+        $(".Kendo").slideDown(8000);   
+    })
+})
+
