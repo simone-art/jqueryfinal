0c8855adf52410c06ddaab169eb57a1624b38dba
simone-art <simoneperiodista3@gmail.com>
2019-10-15 12:29:52 -0300

    Fiz exercicio 3 com efeito toggle

---
+++ exercicios/js/page3.js
@@ -0,0 +1,14 @@
+$ (function(){
+
+    $(".btn-primary").click(function(){
+    $(".Shoto").toggle(1000);
+    })
+
+    $(".btn-primary").click(function(){
+        $(".Bakugo").toggle(4000);
+    })
+
+    $(".btn-primary").click(function(){
+        $(".Izuku").toggle(9000);
+    })
+})
