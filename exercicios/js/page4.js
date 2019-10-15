973d386f379a83ae9ee6259d1ca4bd0c098bae49
simone-art <simoneperiodista3@gmail.com>
2019-10-15 14:11:20 -0300

    fiz o exercicio 4 com o efeito slide toggle

--- exercicios/js/page4.js
+++ exercicios/js/page4.js
@@ -1,15 +1,15 @@
 $(function(){
 
     $(".btn-primary").click(function(){
-        $(".Tenya").slideDown(1000);   
+        $(".Tenya").slideToggle(1000);   
     })
 
     $(".btn-primary").click(function(){
-        $(".Momo").slideDown(5000);   
+        $(".Momo").slideToggle(5000);   
     })
 
     $(".btn-primary").click(function(){
-        $(".Kendo").slideDown(8000);   
+        $(".Kendo").slideToggle(8000);   
     })
 })
 
