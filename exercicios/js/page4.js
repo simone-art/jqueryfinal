02edb41833ad6f2d13f7f5f608b7c995f05af8e9
simone-art <simoneperiodista3@gmail.com>
2019-10-16 11:41:48 -0300

    fiz exercicio page 4 removendo e adicionando class

--- exercicios/js/page4.js
+++ exercicios/js/page4.js
@@ -5,5 +5,14 @@ $(function(){
         $(".Momo").slideDown(5000);   
         $(".Kendo").slideDown(8000);  
     })
+    $(".btn-primary").click(function(){
+        $( ".btn-primary" ).remove();
+        $(".btn-primary").removeClass("btn-primary");
+        $(".btn-secundary").addClass("btn-primary");
+        // $(".btn-primary").attr("disable", true);
+        // $(".btn-secundary").attr("enabled", true);
+        
+        
+    })
 })
 
