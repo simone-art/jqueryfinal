fa43541a3a026f52ff2a1d56d99e05fd840752db
simone-art <simoneperiodista3@gmail.com>
2019-10-16 10:11:26 -0300

    fiz exercicio page 3 com efeitos clone e hover

--- exercicios/js/page3.js
+++ exercicios/js/page3.js
@@ -1,14 +1,52 @@
-$ (function(){
+$(function () {
 
-    $(".btn-primary").click(function(){
-    $(".Shoto").toggle(1000);
+    $(".btn-primary").click(function () {
+        $(".Shoto").toggle(1000);
     })
 
-    $(".btn-primary").click(function(){
+    $(".btn-primary").click(function () {
         $(".Bakugo").toggle(4000);
     })
 
-    $(".btn-primary").click(function(){
+    $(".btn-primary").click(function () {
         $(".Izuku").toggle(9000);
     })
-})
+
+
+
+//EN ESTE EJERCICIO PUEDES LLAMAR POR LA CLASE. EN ESTE CASO CON 
+//BUTTONS
+// $ (FUNCTION (){
+//     $("buttons").click(function(){
+// $(".card:eq(0)".toggle(9000); 
+// $(".card:eq(1)".toggle(9000); 
+// $(".card:eq(2)".toggle(9000);  
+// })
+//});
+
+
+    $(".btn-secundary").click(function () {
+        $("html").append($("body").clone(true));
+    });
+
+
+    $(".card-title").hover(
+        function () {
+            $(this).css("color", "yellow");
+        },
+        function () {
+            $(this).css("color", "black");
+        }
+    );
+
+});
+
+
+
+
+// $ (function(){
+
+//         $(".btn-secundary").click(function(){
+//             $(".row").clone().appendTo("html")
+//         })
+//     })
