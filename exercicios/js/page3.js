b724d1fb076dbe495a58619282c8b7c7bfb1e792
simone-art <simoneperiodista3@gmail.com>
2019-10-16 10:53:47 -0300

    fiz exercicio 3 adicionando o método copiar ao inves de clone

--- exercicios/js/page3.js
+++ exercicios/js/page3.js
@@ -14,15 +14,15 @@ $(function () {
 
 
 
-//EN ESTE EJERCICIO PUEDES LLAMAR POR LA CLASE. EN ESTE CASO CON 
-//BUTTONS
-// $ (FUNCTION (){
-//     $("buttons").click(function(){
-// $(".card:eq(0)".toggle(9000); 
-// $(".card:eq(1)".toggle(9000); 
-// $(".card:eq(2)".toggle(9000);  
-// })
-//});
+    //EN ESTE EJERCICIO PUEDES LLAMAR POR LA CLASE. EN ESTE CASO CON 
+    //BUTTONS
+    // $ (FUNCTION (){
+    //     $("buttons").click(function(){
+    // $(".card:eq(0)".toggle(9000); 
+    // $(".card:eq(1)".toggle(9000); 
+    // $(".card:eq(2)".toggle(9000);  
+    // })
+    //});
 
 
     $(".btn-secundary").click(function () {
@@ -39,14 +39,20 @@ $(function () {
         }
     );
 
-});
-
-
-
-
-// $ (function(){
-
-//         $(".btn-secundary").click(function(){
-//             $(".row").clone().appendTo("html")
-//         })
+})
+//     $(".btn-secundary").click(function () {
+//         $(".original").after($(".original").clone());
 //     })
+// })
+
+// $(".btn-secundary").click(function () {
+//     let content = (".original").html();
+//     $(".original").after("<div class = row>" + content + "</div>")
+// });
+
+//ESTE CÓDIGO SIRVE PARA COPIAR CONTENIDO. ÉSTE ES MÁS ACONSEJABLE QUE EL 
+//CLONE, PORQUE COPIA UNO POR UNO, MIENTRAS EL CLONE MULTIPLICA EL CONTENIDO
+//LA CLASE ORIGINAL LA COLOCAS EN EL HTML DONDE QUIERES QUE EMPIECE A COPIAR EL CONTENIDO Y
+//DEFINES UNA VARIABLE PARA PODER LLAMARLA:let content = (".original").html();
+// Y DESPUÉS IDENTIFICAS LA DIV. $(".original").after("<div class = row>" + content + "</div>") 
+//PARA DECIRLE QUE DESPUÉS DEL AFTER VAS A COPIAR EL CONTENIDO.
