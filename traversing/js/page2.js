b7d8f6bb7a276c239f20b4a803a7fdb28bd7604d
simone-art <simoneperiodista3@gmail.com>
2019-10-17 09:55:06 -0300

    resoluçao dos exercicios page 1 e 2

--- traversing/js/page2.js
+++ traversing/js/page2.js
@@ -3,5 +3,20 @@
 
 $(document).ready(function(){
     $("input:text").parent().addClass("alert alert-info");
-  
-});
+
+    $(".list-group-item").on("click", function (){
+        $(this).removeClass("list-group-item-dark").addClass("list-group-item-info")
+        $(this).siblings().addClass("list-group-item-dark")
+    })
+})
+//OTRA FORMA DE HACER EL EJERCICIO: 
+// $(document).ready(function(){
+//     $("input").filter([type= "text"])parent().addClass("alert alert-info");
+
+// });
+// No arquivo page2.html, faça uma instrução jQuery que selecione o item de lista e:
+
+// Adicione a classe "list-group-item-info" no item.
+// Adicione a classe "list-group-item-dark" nos demais itens.
+
+
